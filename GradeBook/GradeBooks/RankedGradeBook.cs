39bc4497c3f720ceab8a7f65c7cb94a7157293e1
Josiah Smythe <josiahgsmythe@gmail.com>
2020-11-24 10:15:37 -0500

    Completed override of Ranked Grade Books Get letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,20 +13,37 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
+            int studentsGreaterThenInput = 0;
+            char letterGrade = 'F';
             if (Students.Count < 5)
+            {
                 throw new InvalidOperationException();
-            
-            var averageGrades = new List<double>();
+                //throw new InvalidOperationException("Less then five students are in the supplied Gradebook");
+            }
+            else
+            {
+                foreach (var student in Students)
+                {
+                    if (student.AverageGrade > averageGrade)
+                    {
+                        studentsGreaterThenInput++;
+                    }
+                }
+                var twentyPercent = (double)(Students.Count * 0.20);
 
-            foreach (var student in Students)
-                averageGrades.Add(student.AverageGrade);
+                if (studentsGreaterThenInput < twentyPercent)
+                    letterGrade = 'A';
+                else if (studentsGreaterThenInput < (twentyPercent * 2))
+                    letterGrade = 'B';
+                else if (studentsGreaterThenInput < (twentyPercent * 3))
+                    letterGrade = 'C';
+                else if (studentsGreaterThenInput < (twentyPercent * 4))
+                    letterGrade = 'D';
+                else
+                    letterGrade = 'F';
+            }                     
 
-            
-
-
-
-            
-            return 'F';
+            return letterGrade;
         }
     }
 }
