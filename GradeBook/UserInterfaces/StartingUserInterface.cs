525b906d5cf67d957c5cbf85eee36bd4ac4e9446
Josiah Smythe <josiahgsmythe@gmail.com>
2020-11-24 13:36:20 -0500

    Completed refactoring of StartingUserIInterface CreateCommand method.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,18 +40,19 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
-            if (parts[3] == "standard")
+            if (type == "standard")
             { 
                 gradeBook = new StandardGradeBook(name);
             }
-            else if (parts[2] == "ranked")
+            else if (type == "ranked")
             {
                 gradeBook = new RankedGradeBook(name);
             }
             else
             {
-                Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
